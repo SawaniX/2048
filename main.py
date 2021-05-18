3d271eb5e83c3791dfee0bf45d8dcc1ec427c807
SawaniX <TomasZ13>
2021-05-18 22:24:42 +0200

    labele

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import copy
 import random
 import os
 from PySide2.QtWidgets import *
-from PySide2.QtGui import QPen, QPainter, QPolygonF, QBrush, QTextCursor, QFont, QPixmap
-from PySide2.QtCore import Qt, QPointF, QPropertyAnimation, QEvent, QRectF, QObject, Signal, QPoint
+from PySide2.QtGui import QPen, QPainter, QPolygonF, QBrush, QTextCursor, QFont, QPalette
+from PySide2.QtCore import Qt, QPointF, QPropertyAnimation, QEvent, QRectF, QObject, Signal
 
 
 # klasa majaca dane wierzcholkow kazdego pola oraz nr kolumny oraz pozycje w kolumnie
@@ -42,9 +42,11 @@ class Window(QMainWindow):
         self.grid_size = grid_size
         self.var = 2 * self.grid_size - 1
         self.pola = []
+        self.pola2 = []
         self.il_pol = 0
         self.iks = 0
         self.igr = 0
+        self.wynik = 0
         self.szerokosc_sceny = self.var * ((85.98076211353315 + math.sqrt(3) / 2 * 60) - (34.01923788646684 + math.sqrt(3) / 2 * 60))
         self.wysokosc_sceny = self.var * ((115.98076211353315 + math.sqrt(3) / 4 + 43) - (60 + math.sqrt(3) / 4 + 43))
 
@@ -98,6 +100,16 @@ class Window(QMainWindow):
         self.text_box.setGeometry(130,self.wysokosc_sceny*1.02, 250, 150)
         self.text_box.setReadOnly(True)
 
+        self.label1 = QLabel(self)
+        self.label1.setText("Punkty gracza 1:")
+        self.label1.setGeometry(280, 50, 100, 30)
+        self.label1.setFont(QFont('Arial', 10))
+
+        self.label2 = QLabel(self)
+        self.label2.setText(str(self.wynik))
+        self.label2.setGeometry(280, 70, 100, 30)
+        self.label2.setFont(QFont('Arial', 10))
+
         self._createActions()
         self._connectActions()
         self._createMenuBar()
@@ -105,13 +117,21 @@ class Window(QMainWindow):
         self.scene = QGraphicsScene(self)
         self.scene.setBackgroundBrush(QBrush(Qt.gray))
 
+        self.scene2 = QGraphicsScene(self)
+        self.scene2.setBackgroundBrush(QBrush(Qt.gray))
+
         self.create_ui()
+        self.create_ui2()
 
         self.view = QGraphicsView(self.scene, self)
         self.view.setAlignment(Qt.AlignTop and Qt.AlignLeft)
         self.view.setGeometry(0, 20, self.wysokosc_sceny, self.szerokosc_sceny)
         self.view.setMouseTracking(True)
         self.view.viewport().installEventFilter(self)
+
+        self.view2 = QGraphicsView(self.scene2, self)
+        self.view2.setAlignment(Qt.AlignTop and Qt.AlignLeft)
+        self.view2.setGeometry(500, 20, self.wysokosc_sceny, self.szerokosc_sceny)
 
         self.fields = [field(self.pola, self, self.grid_size)]
         self.fields.append(field(self.pola, self, self.grid_size))
@@ -221,6 +241,9 @@ class Window(QMainWindow):
 
         self.text_box.setGeometry(130, self.wysokosc_sceny * 1.02, 250, 150)
 
+        self.label1.setGeometry(self.szerokosc_sceny+40, 50, 100, 30)
+        self.label2.setGeometry(self.szerokosc_sceny+40, 70, 100, 30)
+
         self.scene.clear()
         if self.grid_size == 3:
             self.scene.setSceneRect(-21.296388, 56.645898, 266.515825, 236.421017)
@@ -229,9 +252,19 @@ class Window(QMainWindow):
         elif self.grid_size == 5:
             self.scene.setSceneRect(-73.257913, 56.645898, 474.361922, 410.153068)
 
+        self.scene2.clear()
+        if self.grid_size == 3:
+            self.scene2.setSceneRect(-21.296388, 56.645898, 266.515825, 236.421017)
+        elif self.grid_size == 4:
+            self.scene2.setSceneRect(-47.277150, 56.645898, 370.438874, 323.287042)
+        elif self.grid_size == 5:
+            self.scene2.setSceneRect(-73.257913, 56.645898, 474.361922, 410.153068)
+
         self.create_ui()
+        self.create_ui2()
 
         self.view.setGeometry(0, 20, self.wysokosc_sceny, self.szerokosc_sceny)
+        self.view2.setGeometry(self.szerokosc_sceny * 3-(self.szerokosc_sceny+10), 20, self.wysokosc_sceny, self.szerokosc_sceny)
 
         self.fields.clear()
         self.fields = [field(self.pola, self, self.grid_size)]
@@ -630,6 +663,44 @@ class Window(QMainWindow):
             else:
                 zmienna = zmienna - 1
 
+    def create_ui2(self):
+        blackPen = QPen(Qt.black)
+        blackPen.setWidth(5)
+
+        grid_size = self.grid_size
+        var = self.var
+        x = [60, 85.98076211353315, 85.98076211353315, 60, 34.01923788646684, 34.01923788646684, 60]
+        y = [60, 72.99038105676658, 102.99038105676658, 115.98076211353315, 102.99038105676658, 72.99038105676658, 60]
+        x_cop = copy.deepcopy(x)
+        zmienna = 0
+        k = math.sqrt(3) / 4 + 43
+        for i in range(var):
+            lista = []
+            x = x_cop
+            if i != 0:
+                y = [z + k for z in y]
+            if i < var / 2 and i != 0:
+                x = [z - i*(math.sqrt(3) / 2 * 30) for z in x]
+            if i > var / 2 and i != 0 and i != var-1:
+                x = [z - (var-1-i) * (math.sqrt(3) / 2 * 30) for z in x]
+            for j in range(grid_size+zmienna):
+                if j != 0:
+                    x = [z + math.sqrt(3) / 2 * 60 for z in x]
+                self.scene2.addLine(x[0], y[0], x[1], y[1], blackPen)
+                self.scene2.addLine(x[1], y[1], x[2], y[2], blackPen)
+                self.scene2.addLine(x[2], y[2], x[3], y[3], blackPen)
+                self.scene2.addLine(x[3], y[3], x[4], y[4], blackPen)
+                self.scene2.addLine(x[4], y[4], x[5], y[5], blackPen)
+                self.scene2.addLine(x[5], y[5], x[0], y[0], blackPen)
+                lista.append(plansza(x[0], x[1], x[2], x[3], x[4], x[5], y[0], y[1], y[2], y[3], y[4], y[5], i, j))
+                self.il_pol += 1
+            self.pola2.append(lista)
+
+            if (i < var/2-1):
+                zmienna += 1
+            else:
+                zmienna = zmienna - 1
+
     def add_pol(self, item):
         self.scene.addItem(item)
 
@@ -716,6 +787,10 @@ class Window(QMainWindow):
     def __del__(self):
         sys.stdout = sys.__stdout__
 
+    def akt(self, wyn):
+        self.wynik = self.wynik + wyn
+        self.label2.setText(str(self.wynik))
+
 
 class Stream(QObject):
     box = Signal(str)
@@ -728,52 +803,6 @@ class Stream(QObject):
     def flush(self):
         pass
 
-
-class kloc(QGraphicsItem):
-    def __init__(self, pola, window, grid_size, parent=None):
-        super(kloc, self).__init__(parent)
-        self.pola = pola
-        self.grid_size = grid_size
-        self.window = window
-        self.value = 2
-        self.nmb = self.rand_field()
-        #self.fld, self.txt = self.narysuj_klocek()
-
-    def hexagon(self):
-        n = 6
-        r = 25
-        s = 0
-        w = 360 / n
-        #greenBrush = QBrush(Qt.green)
-        polygon = QPolygonF()
-        wsp_x = []
-        wsp_y = []
-        for i in range(n):  # add the points of polygon
-            t = w * i + s
-            x = (r + 3) * math.cos(math.radians(t) + math.radians(30))
-            y = (r + 0.8) * math.sin(math.radians(t) + math.radians(30))
-            x = x + self.pola[self.nmb[0]][self.nmb[1]].srodek_x
-            y = y + self.pola[self.nmb[0]][self.nmb[1]].srodek_y
-            wsp_x.append(x)
-            wsp_y.append(y)
-            polygon.append(QPointF(x, y))
-        return polygon
-
-    def boundingRect(self):
-        return QRectF(0, 0, 2*25*2, 1.7*2*25)
-
-    def paint(self, painter, option, widget):
-        painter.setBrush(QBrush(Qt.green))
-        painter.setPen(QPen(Qt.black, 3, Qt.SolidLine))
-        painter.drawPolygon(self.hexagon())
-
-    def rand_field(self):
-        free = free_fields(self.pola)
-        leng = len(free)
-        ran = random.randint(0, leng-1)
-        row, col = free[ran]
-        self.pola[row][col].zajety = True
-        return [row, col]
 
 # klasa do klockow
 class field(QGraphicsItem):
@@ -792,38 +821,37 @@ class field(QGraphicsItem):
         ran = random.randint(0, leng-1)
         row, col = free[ran]
         self.pola[row][col].zajety = True
+
+        ran2 = random.randint(0, 10)
+        if ran2 == 7:
+            self.value = 4
+
         return [row, col]
 
     def narysuj_klocek(self):
-        # n = 6
-        # r = 25
-        # s = 0
-        # w = 360 / n
-        # greenBrush = QBrush(Qt.green)
-        # polygon = QPolygonF()
-        # wsp_x = []
-        # wsp_y = []
-        # for i in range(n):  # add the points of polygon
-        #     t = w * i + s
-        #     x = (r + 3) * math.cos(math.radians(t) + math.radians(30))
-        #     y = (r + 0.8) * math.sin(math.radians(t) + math.radians(30))
-        #     x = x + self.pola[self.nmb[0]][self.nmb[1]].srodek_x
-        #     y = y + self.pola[self.nmb[0]][self.nmb[1]].srodek_y
-        #     wsp_x.append(x)
-        #     wsp_y.append(y)
-        #     polygon.append(QPointF(x, y))
-        # xd = QGraphicsPolygonItem(polygon)
-        # xd.setBrush(greenBrush)
-
-
-        #txt = QGraphicsTextItem(str(self.value), xd)
-        #txt.setPos(self.pola[self.nmb[0]][self.nmb[1]].srodek_x-6, self.pola[self.nmb[0]][self.nmb[1]].srodek_y-9)
-        #self.window.add_pol(xd)
-        item = kloc(self.pola, self.window, self.grid_size)
-        txt = QGraphicsTextItem(str(self.value), item)
-        txt.setPos(self.pola[self.nmb[0]][self.nmb[1]].srodek_x - 6, self.pola[self.nmb[0]][self.nmb[1]].srodek_y - 9)
-        self.window.add_pol(item)
-        return item, txt
+        n = 6
+        r = 25
+        s = 0
+        w = 360 / n
+        greenBrush = QBrush(Qt.green)
+        polygon = QPolygonF()
+        wsp_x = []
+        wsp_y = []
+        for i in range(n):  # add the points of polygon
+            t = w * i + s
+            x = (r + 3) * math.cos(math.radians(t) + math.radians(30))
+            y = (r + 0.8) * math.sin(math.radians(t) + math.radians(30))
+            x = x + self.pola[self.nmb[0]][self.nmb[1]].srodek_x
+            y = y + self.pola[self.nmb[0]][self.nmb[1]].srodek_y
+            wsp_x.append(x)
+            wsp_y.append(y)
+            polygon.append(QPointF(x, y))
+        xd = QGraphicsPolygonItem(polygon)
+        xd.setBrush(greenBrush)
+        txt = QGraphicsTextItem(str(self.value), xd)
+        txt.setPos(self.pola[self.nmb[0]][self.nmb[1]].srodek_x-6, self.pola[self.nmb[0]][self.nmb[1]].srodek_y-9)
+        self.window.add_pol(xd)
+        return xd, txt
 
     def zmien_pozycje_pg(self):
         x_mov = (self.pola[0][1].srodek_x - self.pola[0][0].srodek_x) / 2
@@ -982,6 +1010,7 @@ class field(QGraphicsItem):
 
     def upd_text(self):
         self.txt.setPlainText(str(self.value))
+        self.window.akt(self.value)
         if self.value == 16:
             self.txt.moveBy(-4, 0)
         elif self.value == 128:
